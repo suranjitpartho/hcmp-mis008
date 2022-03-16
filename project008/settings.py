6c972a2af7b6b1c8248f46d6c2600c86580bb54b
Suranjit Das <suranjitpartho@gmail.com>
2022-03-16 13:45:12 +0600

    day two one

--- project008/settings.py
+++ project008/settings.py
@@ -3,7 +3,7 @@ import os
 # Paths:
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,4 +113,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_ROOT,]
+STATICFILES_DIRS = [STATIC_DIR,]
