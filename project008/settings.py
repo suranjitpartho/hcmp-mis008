e7e31df04d2a974361556c9bac8dc5d4a9ee81bc
Suranjit Das <suranjitpartho@gmail.com>
2022-03-16 14:28:07 +0600

    day two two

--- project008/settings.py
+++ project008/settings.py
@@ -2,8 +2,13 @@ import os
 
 # Paths:
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+
 
 
 # Quick-start development settings - unsuitable for production
--- project008/wsgi.py
+++ project008/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
